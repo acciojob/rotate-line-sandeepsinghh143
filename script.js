55cfba6f3062d19e5703c6bb2b8245975cefdacb
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-05 14:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let angle = 0;
+const rotationSpeed = 1; // Adjust this value to control the rotation speed
+
+function rotateLine() {
+  const line = document.getElementById('line');
+  angle += rotationSpeed;
+  line.style.transform = `rotate(${angle}deg)`;
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine()
